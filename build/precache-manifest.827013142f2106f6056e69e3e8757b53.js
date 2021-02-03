1d37a12da80ab159078a362abe0946187a3c1899
Muhammad Saad <itsmsaadhere@gmail.com>
2021-02-03 19:26:05 +0500

    build

---
+++ build/precache-manifest.827013142f2106f6056e69e3e8757b53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4932a55926e69dc511ca",
+    "url": "/static/css/main.cb193afd.chunk.css"
+  },
+  {
+    "revision": "4932a55926e69dc511ca",
+    "url": "/static/js/main.4932a559.chunk.js"
+  },
+  {
+    "revision": "fd8ecc4068f8f9e4d75c",
+    "url": "/static/js/1.fd8ecc40.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "bc9fb2ebc8a99094b4bdbd45249ea2be",
+    "url": "/index.html"
+  }
+];
